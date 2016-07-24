5b0218a746222222d8d097f8d5fd0a00d643cc98
Bernardo Gomes <bgomesdeabreu@gmail.com>
2016-07-24 16:14:32 -0300

    quickstart

---
+++ angular2-quickstart/app/app.component.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by Bernardo on 24/07/2016.
+ */
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>My Second Angular 2 App</h1>'
+})
+export class AppComponent { }
---
+++ readme.md
@@ -0,0 +1,3 @@
+#Estudos de Angular2
+
+1. [QuickStart https://angular.io/docs/ts/latest/quickstart.html](angular2-quickstart)
